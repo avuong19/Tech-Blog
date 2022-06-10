c42267dda54e6e0043c8632f94eef0c1a19ccea3
Anh Vuong <vta19797@gmail.com>
2022-06-09 19:24:32 -0500

    set up relations between tables

--- models/index.js
+++ models/index.js
@@ -11,4 +11,38 @@ Post.belongsTo(User, {
     onDelete: 'SET NULL'
   });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+  
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
 module.exports = { User, Comment, Post };
