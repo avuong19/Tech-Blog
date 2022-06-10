f91a4bd81bf85a9d5694ce1db9e7ad1a188785e5
Anh Vuong <vta19797@gmail.com>
2022-06-09 19:22:00 -0500

    set up models tables

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,2 +1,38 @@
 const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
+const { User, Post, Comment } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+      attributes: { exclude: ['password'] }
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,14 @@
+const User = require('./User');
+const Comment=require('./Comment');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+
+module.exports = { User, Comment, Post };
